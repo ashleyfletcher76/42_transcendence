146a42fc125dd294a94c8e6c151f1d2220d6bbee
Ashley Fletcher <asfletch@2-A-6.42heilbronn.de>
2024-11-05 15:44:17 +0100

    Separate database created for each django service now. Currently working and checked all the logs

--- backend/chat-service/config/settings.py
+++ backend/chat-service/config/settings.py
@@ -88,13 +88,13 @@ CHANNEL_LAYERS = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DB"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": "db",
+        "NAME": os.environ.get("CHAT_SERVICE_DB"),
+        "USER": os.environ.get("CHAT_SERVICE_USER"),
+        "PASSWORD": os.environ.get("CHAT_SERVICE_PASSWORD"),
+        "HOST": "chat-db",
         "PORT": "5432",
         "OPTIONS": {
-            "sslmode": "require",  # we use this to enforce ssl on the database also
+            # "sslmode": "require",  # we use this to enforce ssl on the database also
         },
     }
 }
@@ -103,13 +103,13 @@ DATABASES = {
 if "test" in sys.argv:
     DATABASES["default"] = {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "test_" + os.environ.get("POSTGRES_DB"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": "db",
+        "NAME": "test_" + os.environ.get("CHAT_SERVICE_DB"),
+        "USER": os.environ.get("CHAT_SERVICE_USER"),
+        "PASSWORD": os.environ.get("CHAT_SERVICE_PASSWORD"),
+        "HOST": "chat-db",
         "PORT": "5432",
         "OPTIONS": {
-            "sslmode": "require",  # we use this to enforce ssl on the database also
+            # "sslmode": "require",  # we use this to enforce ssl on the database also
         },
     }
 
