97c024622941030e3f827f53114b89ec1900b53d
Muhammet Kose <muhakose@2-A-4.42heilbronn.de>
2024-11-19 11:46:32 +0100

    Default_msg

--- backend/lobby/config/settings.py
+++ backend/lobby/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "lobby",
+    "daphne",
 ]
 
 MIDDLEWARE = [
@@ -70,6 +71,7 @@ TEMPLATES = [
 ]
 
 #WSGI_APPLICATION = "config.wsgi.application"
+ASGI_APPLICATION = "config.asgi.application"
 
 WSGI_APPLICATION = "config.wsgi.application"
 
