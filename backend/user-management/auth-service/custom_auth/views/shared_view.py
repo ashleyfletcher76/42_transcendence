efc47bc2b602d30d89eaf887de6f9f7abb3f0012
Ashley Fletcher <asfletch@3-F-11.42heilbronn.de>
2025-01-11 18:15:06 +0100

    Fixed the response for loginview

--- backend/user-management/auth-service/custom_auth/views/shared_view.py
+++ backend/user-management/auth-service/custom_auth/views/shared_view.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework import status
 
-def generate_tokens_and_status(self, user_data):
+def generate_tokens_and_status(user_data):
 	"""Generate the above items"""
 
 	user_id = user_data["user_id"]
